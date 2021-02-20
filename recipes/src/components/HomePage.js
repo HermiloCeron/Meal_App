edd48d36cf51bf3b48917289e42255d44c3ab726
ceronhermilo <ceronhermilo@yahoo.com>
2021-02-19 20:44:57 -0600

    make HomePage a react Component

--- recipes/src/components/HomePage.js
+++ recipes/src/components/HomePage.js
@@ -1,50 +1,58 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const HomePage=(props)=>{
+class HomePage extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
+  }
   // console.log(props.areas);
   // console.log(props.categories);
   // console.log(props.ingredients);
-  return(
-    <div>
-      <div>Inside HomePage</div>
+  render(){
+    return(
+      <div>
+        <div>Inside HomePage</div>
 
-      <div>Search by category</div>
+        <div>Search by category</div>
 
-      <form onSubmit={(e)=>{props.searchCategory(e)}} >
-        <label htmlFor='category'> </label>
-        <select name='category'>
-          {props.categories.map((category,index)=>(
-            <option
-              key={'category-seacrh-option-'+index}
-              value={props.categories[index]}
-            >
-              {props.categories[index]}
-            </option>
-          ))}
-        </select>
-        <input type='submit' value='Search'/>
-      </form>
+        <form onSubmit={(e)=>{this.props.searchCategory(e)}} >
+          <label htmlFor='category'> </label>
+          <select name='category'>
+            {this.props.categories.map((category,index)=>(
+              <option
+                key={'category-seacrh-option-'+index}
+                value={this.props.categories[index]}
+              >
+                {this.props.categories[index]}
+              </option>
+            ))}
+          </select>
+          <input type='submit' value='Search'/>
+        </form>
 
 
-      <div>Search by area</div>
+        <div>Search by area</div>
 
-      <form onSubmit={(e)=>{props.searchArea(e)}} >
-        <label htmlFor='area'> </label>
-        <select name='area'>
-          {props.areas.map((area,index)=>(
-            <option
-              key={'area-seacrh-option-'+index}
-              value={props.areas[index]}
-            >
-              {props.areas[index]}
-            </option>
-          ))}
-        </select>
-        <input type='submit' value='Search'/>
-      </form>
+        <form onSubmit={(e)=>{this.props.searchArea(e)}} >
+          <label htmlFor='area'> </label>
+          <select name='area'>
+            {this.props.areas.map((area,index)=>(
+              <option
+                key={'area-seacrh-option-'+index}
+                value={this.props.areas[index]}
+              >
+                {this.props.areas[index]}
+              </option>
+            ))}
+          </select>
+          <input type='submit' value='Search'/>
+        </form>
 
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 export default HomePage;
