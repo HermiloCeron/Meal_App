13432f6dd76e793146e5fbd3e757c7e8db3a90f5
ceronhermilo <ceronhermilo@yahoo.com>
2021-02-19 19:30:11 -0600

    Create form to select meal category

--- recipes/src/components/HomePage.js
+++ recipes/src/components/HomePage.js
@@ -5,7 +5,24 @@ const HomePage=(props)=>{
   console.log(props.categories);
   console.log(props.ingredients);
   return(
-    <div>Inside HomePage</div>
+    <div>
+      <div>Inside HomePage</div>
+      <div>Search by category</div>
+      <form>
+        <label htmlFor='category'> </label>
+        <select name='category'>
+          {props.categories.map((category,index)=>(
+            <option
+              key={'category-seacrh-option-'+index}
+              value={props.categories[index]}
+            >
+              {props.categories[index]}
+            </option>
+          ))}
+        </select>
+        <input type='submit' value='Search'/>
+      </form>
+    </div>
   )
 }
 
