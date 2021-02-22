17bb7db75ab33db347e66042847e348f34862032
MattG2000 <GuffyMatthewO@JohnDeere.com>
2021-02-22 11:07:04 -0500

    add searchResults code

--- recipes/src/components/SearchResults.js
+++ recipes/src/components/SearchResults.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SearchResults=(props)=>{
-  return(
-    <div>Inside SearchResults</div>
+const SearchResults = props => {
+    
+  return (
+      <div>
+          { props.mealResults.map(park => (
+              <div className='grid-list' key={park.ParkCode}>
+                  <Link to={`/park/${park.id}`}>
+                      {park.images[0] && <img className='grid-image' src={park.images[0].url} alt='National Park' />}
+                  <h2>{park.name}</h2>
+                  </Link>
+              </div>
+
+          )) }
+      </div>
   )
+
 }
+
+
+
+// const SearchResults=(props)=>{
+//   return(
+//     <div>Inside SearchResults</div>
+//   )
+// }
 
 export default SearchResults;
