6de45e1a7ecfdcf8dc8e76bce484e760d2d50f48
hermiloceron <>
2021-02-23 11:54:34 -0600

    Some minnor changes to the aside

--- recipes/src/components/SideImage.js
+++ recipes/src/components/SideImage.js
@@ -3,12 +3,12 @@ import React from 'react';
 const SideImage=(props)=>{
   return(
     <div>
-      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+      <div id="side-carousel" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner">
           {props.randomMealArray.map((meal,index)=>(
             <div
               className={index==0 ? "carousel-item active" : "carousel-item"}
-              key={"image"+meal.idMeal}
+              key={index+"-image-"+meal.idMeal}
               onClick={(e) => {props.selectMealById(e, meal.idMeal)}}
             >
               <img src={meal.strMealThumb} className="d-block w-25" alt="..." />
